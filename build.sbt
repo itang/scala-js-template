cff7dbed722738940835a0d58e975c53c2d34617
itang <live.tang@gmail.com>
2017-11-19 10:24:13 +0800

    cleanup

--- build.sbt
+++ build.sbt
@@ -1,19 +1,20 @@
 name := "Example"
 
-scalaVersion := "2.11.8"
+scalaVersion := "2.12.4"
 
 version := "0.1-SNAPSHOT"
 
-libraryDependencies ++= Seq(
- "org.monifu" %%% "minitest" % "0.12" % "test"
-)
-
-testFrameworks += new TestFramework("minitest.runner.Framework")
-
-// Turn this project into a Scala.js project by importing these settings
-
 enablePlugins(ScalaJSPlugin)
 
-persistLauncher := true
+scalaJSUseMainModuleInitializer := true
+// scalaJSStage in Global := FullOptStage
 
-persistLauncher in Test := false
+libraryDependencies ++= Seq(
+  "org.scala-js" %%% "scalajs-dom" % "0.9.1",
+  "be.doeraene" %%% "scalajs-jquery" % "0.9.1",
+  "io.monix" %% "minitest" % "2.0.0" % "test"
+)
+
+jsDependencies += "org.webjars" % "jquery" % "2.1.4" / "2.1.4/jquery.js"
+
+testFrameworks += new TestFramework("minitest.runner.Framework")
--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -1,27 +1,35 @@
 package example
 
+import org.scalajs.dom.{document => g}
+
 import scala.scalajs.js
-import js.Dynamic.{ global => g }
-import js.annotation.JSExport
-import js.annotation.JSExportDescendentObjects
+import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
+import org.scalajs.jquery.jQuery
 
-object ScalaJSExample extends js.JSApp {
 
-  def main(): Unit = {
-    val paragraph = g.document.createElement("p")
+@JSExportTopLevel("example.ScalaJSExample")
+object ScalaJSExample {
+
+  def main(_args: Array[String]): Unit = {
+    val paragraph = g.createElement("p")
     val s = "hello"
     paragraph.innerHTML = s"<strong>It works!</strong> ${s} ${new js.Date()}"
     $("playground").appendChild(paragraph)
+
+    jQuery("body").append("<div><h2>Append from jQuery</div></h2>")
   }
 
   /**
-   * Computes the square of an integer.
-   *  This demonstrates unit testing.
-   */
+    * Computes the square of an integer.
+    * This demonstrates unit testing.
+    */
   @JSExport
-  def square(x: Int): Int = { println("square"); x * x }
+  def square(x: Int): Int = {
+    println("square")
+    x * x
+  }
 }
 
 object $ {
-  def apply(id: String) = g.document.getElementById(id)
+  def apply(id: String) = g.getElementById(id)
 }
